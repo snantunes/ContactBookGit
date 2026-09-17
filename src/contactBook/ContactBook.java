a68ae22f310481ec8065d426aa3588f3cdf491fa
Salvador Antunes <sn.antunes@campus.fct.unl.pt>
2026-09-17 22:27:24 +0100

    Fixed loop warning

--- src/contactBook/ContactBook.java
+++ src/contactBook/ContactBook.java
@@ -103,7 +103,7 @@ public class ContactBook {
     }
 
     private void resize() {
-        Contact tmp[] = new Contact[2*contacts.length];
+        Contact[] tmp = new Contact[2*contacts.length];
         for (int i=0;i<counter; i++)
             tmp[i] = contacts[i];
         contacts = tmp;
